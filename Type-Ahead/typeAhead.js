7095d25d3269709f00a150d535736482af495fe6
Shriram <shriramsinbox@gmail.com>
2018-11-18 04:00:08 -0600

    star rating widget

--- Type-Ahead/typeAhead.js
+++ Type-Ahead/typeAhead.js
@@ -1,3 +1,4 @@
+// #airbnb
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
 const cities = [];
@@ -47,3 +48,4 @@ const suggestions = document.querySelector('.suggestions');
 
 
 searchInput.addEventListener('keyup', displayMatches);
+
